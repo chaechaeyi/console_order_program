71305635030e71f1c553fd69c67b72e733736932
moya-yi <hhh00613@gmail.com>
2023-06-07 05:15:06 +0900

    #3 스프링 부트 프로젝트 시작하기 스프링 부트 초기화 들어간 기술 * 스프링 웹 * 엑츄에이터 * 데브툴즈 * 롬복

---
+++ src/test/java/kr/co/_29cm/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co._29cm.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
