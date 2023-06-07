d6807514ec9e4011274449cce777ea36ee852c44
moya-yi <hhh00613@gmail.com>
2023-06-08 02:38:45 +0900

    #1 주문 프로그램 api 만들기 Sprig Data REST + HAL Explorer dependency 추가 api를 간편하게 만들기 위해서 스프링 부트 기능 추가

---
+++ src/test/java/kr/co/_29cm/homework/controller/DataRestTest.java
@@ -0,0 +1,39 @@
+package kr.co._29cm.homework.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest API 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 상품 리스트 조회")
+    @Test
+    void givenNothing_wheRequestItems_then() throws Exception {
+        //Given
+        //When & Then
+        mvc.perform(get("/api/items"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+}
