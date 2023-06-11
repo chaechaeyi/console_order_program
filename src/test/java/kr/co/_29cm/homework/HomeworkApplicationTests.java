97e6602288458c52430918d7769521aa9de39e4f
moya-yi <hhh00613@gmail.com>
2023-06-11 19:31:40 +0900

    build 가능한 형태로 수정

--- src/test/java/kr/co/_29cm/homework/HomeworkApplicationTests.java
+++ src/test/java/kr/co/_29cm/homework/HomeworkApplicationTests.java
@@ -2,7 +2,9 @@ package kr.co._29cm.homework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HomeworkApplicationTests {
 
